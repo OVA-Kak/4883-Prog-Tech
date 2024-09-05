9bdcae137962eb0acf65f2492f3b71e341f3a6cf
OVA-Kak <zecha.charlie@gmail.com>
2024-09-05 08:49:36 -0500

    Add files via upload

---
+++ assignments/P10055/main.cpp
@@ -0,0 +1,20 @@
+/**
+* Charles Zecha
+* 4883 Prog Tech
+* 9/5/2024
+*/
+#include <iostream>
+
+#define endl "\n"
+
+using namespace std;
+
+int main() {
+	long B = 0, A = 0;
+
+	while (cin >> A >> B) {
+
+		cout << abs(A - B) << endl;
+	}
+	return 0;
+}
